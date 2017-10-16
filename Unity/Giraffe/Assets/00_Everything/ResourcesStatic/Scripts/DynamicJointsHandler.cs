275d7a108498c8b98e153c7e7e54b32078012ac4
Eddie Lee <eddie@funktroniclabs.com>
2017-10-15 22:48:10 -0700

    neck spring

---
+++ Unity/Giraffe/Assets/00_Everything/ResourcesStatic/Scripts/DynamicJointsHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicJointsHandler : MonoBehaviour 
+{
+    public GameObject[] DynamicJoints;
+
+    static GameObject RootDynamicJoints = null;
+
+    [Tooltip("Attach to the parent of this transform")]
+    public Transform attachToParentOfTransform = null;
+
+    private void OnEnable()
+    {
+        if (RootDynamicJoints == null)
+            RootDynamicJoints = new GameObject("DynamicJoints");
+
+        var parent = RootDynamicJoints.transform;
+
+        if (attachToParentOfTransform != null)
+            parent = attachToParentOfTransform.parent;
+
+        for (int i = 0; i < DynamicJoints.Length; ++i)
+        {
+            var obj = DynamicJoints[i];
+
+            if (obj == null)
+                continue;
+
+            obj.transform.SetParent(parent, true);
+
+            var joint = obj.GetComponent<ConfigurableJoint>();
+            if (joint != null)
+                joint.connectedAnchor = joint.connectedAnchor;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var obj in DynamicJoints)
+            GameObject.Destroy(obj);
+
+        DynamicJoints = null;
+    }
+}
